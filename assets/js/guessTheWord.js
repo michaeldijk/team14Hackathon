a7f50ba573dc91c88366f6f8ca4d8ce4cfaedc7b
michaeldijk <michaeldijk@outlook.com>
2021-03-05 09:53:52 +0000

    added remaining code for the game

--- assets/js/guessTheWord.js
+++ assets/js/guessTheWord.js
@@ -1,4 +1,4 @@
-// default variables
+    // default variables
     const maxGuess = 10; // maximum 10 guesses per game easy
     let pauseGame = false;
     let lettersGuessed = []; //emtpy variable to store the letters used for guessing
@@ -33,3 +33,102 @@
     ]; // done
 
     resetGame();
+
+    // Function / option when key is pressed, and making sure it's an alphabetic Character
+    document.onkeypress = function (event) {
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+    function isAlpha(ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    // Update the document with the information needed, for the game
+    function updateHtmlDisplay() {
+        document.getElementById("totalWins").innerText = totalWins; // total wins update
+        document.getElementById("wordHint").innerText = wordHint; // Word hint update
+        document.getElementById("currentWord").innerText = wordToGuess.join(""); // Guessing word update
+        document.getElementById("remainingGuesses").innerText = numbersGuessed; // Number of guesses done
+        document.getElementById("guessedLetters").innerText = lettersGuessed.join(" "); // Adding space between letters guessed
+    }
+
+    // Function which is called at the beginning of the game, once key is pressed, then values reset to null/zero, to start the game
+    function resetGame() {
+        numbersGuessed = maxGuess;
+        pauseGame = false;
+
+        // Get a new word and hint from the array of values.
+        let randomWordFromArray = Math.floor(Math.random() * possibleOptions.length);
+        // Take word to use for game, and store inside "wordToUseForMatch"
+        wordToUseForMatch = possibleOptions[randomWordFromArray][0].toUpperCase()
+        console.log(wordToUseForMatch);
+        // Take a hint to use for game, and store inside "wordHint"
+        wordHint = possibleOptions[randomWordFromArray][1].toUpperCase()
+        console.log(wordHint);
+
+
+        // Reset arrays for new game
+        lettersGuessed = [];
+        wordToGuess = [];
+
+        // Reset the guessed word
+        for (var i = 0, j = wordToUseForMatch.length; i < j; i++) {
+            if (wordToUseForMatch[i] === " ") {
+                wordToGuess.push(" ");
+            } else {
+                wordToGuess.push("_");
+            }
+        }
+
+        // Update the Display
+        updateHtmlDisplay();
+    }
+
+    // Game Functions
+    // Check if letter is in word & process, play sound, either positive or negative.
+    function checkForLetter(letter) {
+        var foundLetter = false;
+        var correctSound = document.createElement("audio");
+        var incorrectSound = document.createElement("audio");
+        correctSound.setAttribute("src", "assets/sounds/positive.wav");
+        incorrectSound.setAttribute("src", "assets/sounds/negative.wav");
+
+        // Search string for letter
+        for (var i = 0, j = wordToUseForMatch.length; i < j; i++) {
+            if (letter === wordToUseForMatch[i]) {
+                wordToGuess[i] = letter;
+                foundLetter = true;
+                correctSound.play();
+                // If guessing word matches random word
+                if (wordToGuess.join("") === wordToUseForMatch) {
+                    // Add to # of wins
+                    totalWins++;
+                    pauseGame = true;
+                    updateHtmlDisplay();
+                    setTimeout(resetGame, 5000);
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            incorrectSound.play();
+            // Check if inccorrect guess is already on the list
+            if (!lettersGuessed.includes(letter)) {
+                // Add incorrect letter to guessed letter list
+                lettersGuessed.push(letter);
+                // Decrement the number of remaining guesses
+                numbersGuessed--;
+            }
+            if (numbersGuessed === 0) {
+                // Display word before reseting game
+                wordToGuess = wordToUseForMatch.split();
+                pauseGame = true;
+                setTimeout(resetGame, 5000);
+            }
+        }
+
+        updateHtmlDisplay();
+
+    }
