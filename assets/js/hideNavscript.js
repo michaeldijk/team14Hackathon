ebb23a6af456c0c32095d892ed093491a02630ee
Toto-Kotaro-Tanaka <ktntrtmt@gmail.com>
2021-03-07 20:05:41 +0000

    Update README.md, style h1 on index.html and refactor html and css

--- assets/js/hideNavscript.js
+++ assets/js/hideNavscript.js
@@ -5,33 +5,30 @@ const navSlide = () => {
 
     burguer.addEventListener("click", () => {
         nav.classList.toggle('nav-active');
-
     })
 
     for (i = 0; i < navClose.length; i++) {
-    navClose[i].addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-    });
-  }
+        navClose[i].addEventListener("click", () => {
+            nav.classList.toggle("nav-active");
+        });
+    }
 }
 
 navSlide()
-
 
 //=====================Back to Top button============================//
 const backToTopButton = document.querySelector("#back-to-top-btn");
 window.addEventListener("scroll", scrollFunction);
 function scrollFunction() {
-  if (window.pageYOffset > 200) { // Show backToTopButton
-      backToTopButton.style.display = "block";
+    if (window.pageYOffset > 200) { // Show backToTopButton
+        backToTopButton.style.display = "block";
     }
-  else { // Hide backToTopButton
-      backToTopButton.style.display = "none";
+    else { // Hide backToTopButton
+        backToTopButton.style.display = "none";
     };
 }
 backToTopButton.addEventListener("click", BackToTop);
-function BackToTop(){
-  window.scroll(0, 0);
+function BackToTop() {
+    window.scroll(0, 0);
 }
-
 //=====================End Back to Top button============================//
